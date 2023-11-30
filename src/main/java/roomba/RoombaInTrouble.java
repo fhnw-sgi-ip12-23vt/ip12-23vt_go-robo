4330ba6a1d53a2aecdb7d4d7e5faf4b8bef5fc87
jansc <jan.schoeni@students.fhnw.ch>
2023-11-30 08:35:06 +0100

    New Obstacle Images, New Goal Image, Rule to randomize Obstacle Images

--- src/main/java/roomba/RoombaInTrouble.java
+++ src/main/java/roomba/RoombaInTrouble.java
@@ -4,8 +4,6 @@ import processing.core.PApplet;
 
 public class RoombaInTrouble {
     public static void main(String[] args) {
-
-
         PApplet.main(new String[] { GameField.class.getName() });
     }
 }
