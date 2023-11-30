2bca486eed3780021cc9bf13e8e617e2f509d023
M.vanderKolk.inf19 <M.vanderKolk.inf19@stud.bbbaden.ch>
2023-11-30 14:15:15 +0100

    added imageLoader

--- src/main/java/roomba/Goal.java
+++ src/main/java/roomba/Goal.java
@@ -7,11 +7,12 @@ public class Goal extends AnimatedSprite {
 	public Goal(PApplet pApplet, PImage img, float scale) {
 		super(pApplet, img, scale);
 		standNeutral = new PImage[4];
-        //TODO change img
-		standNeutral[0] = pApplet.loadImage("../../resources/img/goal/battery-frame0.png");
-		standNeutral[1] = pApplet.loadImage("../../resources/img/goal/battery-frame1.png");
-		standNeutral[2] = pApplet.loadImage("../../resources/img/goal/battery-frame2.png");
-		standNeutral[3] = pApplet.loadImage("../../resources/img/goal/battery-frame3.png");
+		
+		standNeutral[0] = ImageLoader.loadImage(pApplet, "img/goal/battery-frame0.png");
+        standNeutral[1] = ImageLoader.loadImage(pApplet, "img/goal/battery-frame1.png");
+        standNeutral[2] = ImageLoader.loadImage(pApplet, "img/goal/battery-frame2.png");
+        standNeutral[3] = ImageLoader.loadImage(pApplet, "img/goal/battery-frame3.png");
+        
 		currentImages = standNeutral;
 	}
 }
